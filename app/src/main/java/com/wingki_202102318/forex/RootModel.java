48971a39909980139e022cf6df7fcb5f3a389c78
Wingki <wingkisandros@gmail.com>
2023-06-22 10:46:30 +0700

    Initial commit

---
+++ app/src/main/java/com/wingki_202102318/forex/RootModel.java
@@ -0,0 +1,13 @@
+package com.wingki_202102318.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RootModel {
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+    public RootModel(){}
+
+    public RatesModel getRatesModel(){return ratesModel;}
+
+    public void setRatesModel(RatesModel ratesModel){this.ratesModel = ratesModel;}
+}
